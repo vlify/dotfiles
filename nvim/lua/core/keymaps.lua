eb57b64690c550a67c83d86099c90a0130bd2a78
ent <ent@entdeMacBook.local>
2025-09-13 12:16:01 +0800

    change beffer swap button

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -19,8 +19,8 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- 切换buffer
-keymap.set("n", "<C-L>", ":bnext<CR>")
-keymap.set("n", "<C-H>", ":bprevious<CR>")
+keymap.set("n", "<S-L>", ":bnext<CR>")
+keymap.set("n", "<S-H>", ":bprevious<CR>")
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
