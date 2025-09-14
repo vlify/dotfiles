760a64c38f870403e4bad8efb8479a06dc5d092f
ent <ent@entdeMacBook.local>
2025-09-15 00:05:42 +0800

    add configration of java and fix the comment

--- nvim/lua/plugins/plugins-setup.lua
+++ nvim/lua/plugins/plugins-setup.lua
@@ -2,7 +2,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
-    "clone",
+
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
@@ -23,29 +23,55 @@ local plugins = {
 
   {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim", -- 这个相当于mason.nvim和lspconfig的桥梁
+    "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig"
   },
 
       -- 自动补全
   "hrsh7th/nvim-cmp",
   "hrsh7th/cmp-nvim-lsp",
-  "L3MON4D3/LuaSnip", -- snippets引擎，不装这个自动补全会出问题
+  "L3MON4D3/LuaSnip", -- snippets引擎
   "saadparwaiz1/cmp_luasnip",
   "rafamadriz/friendly-snippets",
   "hrsh7th/cmp-path", -- 文件路径
 
-  "numToStr/Comment.nvim", -- gcc和gc注释
+  "numToStr/Comment.nvim", -- 注释
   "windwp/nvim-autopairs", -- 自动补全括号
 
-  "akinsho/bufferline.nvim", -- buffer分割线
-  "lewis6991/gitsigns.nvim", -- 左则git提示
+  "akinsho/bufferline.nvim", -- buffer
+  "lewis6991/gitsigns.nvim", -- git提示
+  'mfussenegger/nvim-dap', -- nvim调试
+  'mfussenegger/nvim-jdtls', -- java调试配置
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}
+  }, -- 调试ui界面
 
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8', -- 文件检索
     dependencies = { {'nvim-lua/plenary.nvim'} } -- requires要改为dependencies
   },
 
+  -- {
+  --   "folke/which-key.nvim", -- 快捷键查询
+  --   dependencies = {{ 'nvim-mini/mini.nvim', version = false },}
+  -- }
+  {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+    dependencies = {{ 'nvim-mini/mini.nvim', version = false },}
+
+}
 }
 local opts = {} -- 注意要定义这个变量
 
