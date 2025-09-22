d7e798085b06b763b4071a33c8b86e3dbbb6dfd1
ryan <vlify@github.com>
2025-09-22 08:58:07 +0800

    add java-cmp support

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,32 +1,35 @@
--- /Users/ent/.config/nvim/ftplugin/java.lua
---
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 local config = {
   name = "jdtls",
-  cmd = {"jdtls"},
-  --
-  -- `root_dir` must point to the root of your project.
-  -- See `:help vim.fs.root`
-  root_dir = vim.fs.root(0, {'gradlew', '.git', 'mvnw','pom.xml'}),
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  cmd = { "jdtls" },
+  -- root_dir = vim.fs.root(0, { "gradlew", ".git", "mvnw", "pom.xml", "build.gradle" }),
+  root_dir = vim.fn.getcwd(),
+
+  capabilities = capabilities,  
 
   settings = {
     java = {
-      format ={
-        enable = true
-      }
-    }
+      completion = {
+        importOrder = { "java", "javax", "com", "org" },
+        importStarThreshold = 9999,
+        guessMethodArguments = true,
+      },
+      contentProvider = { preferred = "fernflower" },
+      sources = {
+        organizeImports = {
+          starThreshold = 9999,
+          staticStarThreshold = 9999,
+        },
+      },
+      signatureHelp = { enabled = true },
+    },
   },
 
-
-  -- This sets the `initializationOptions` sent to the language server
-  -- If you plan on using additional eclipse.jdt.ls plugins like java-debug
-  -- you'll need to set the `bundles`
-  --
-  -- See https://codeberg.org/mfussenegger/nvim-jdtls#java-debug-installation
-  --
-  -- If you don't plan on any eclipse.jdt.ls plugins you can remove this
   init_options = {
-    bundles = {}
+    bundles = {},
   },
 }
-require('jdtls').start_or_attach(config)
+
+require("jdtls").start_or_attach(config)
+
